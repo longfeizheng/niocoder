f11845e11198e102b3a47f8c8be3dd91d9af41b2
niocoder <i@merryyou.cn>
2018-06-06 20:24:06 +0800

    volatile不能保证原子性，只能保证可见性

---
+++ src/test/java/com/niocoder/niocoder/thread/TheadTest.java
@@ -0,0 +1,28 @@
+package com.niocoder.niocoder.thread;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class TheadTest {
+
+    public static AtomicInteger inc = new AtomicInteger(0);
+     
+    public void increase() {
+        inc.incrementAndGet();
+    }
+     
+    public static void main(String[] args) {
+        final TheadTest test = new TheadTest();
+        for(int i=0;i<10;i++){
+            new Thread(){
+                public void run() {
+                    for(int j=0;j<1000;j++)
+                        test.increase();
+                };
+            }.start();
+        }
+         
+        while(Thread.activeCount()>1)  //保证前面的线程都执行完
+            Thread.yield();
+        System.out.println(test.inc);
+    }
+}
